5fc2f8b85cefd37ed071093449517cb473a48226
diana66-git <ds221323@gmail.com>
2026-04-17 15:09:59 -0400

    actualizacion

--- persistencia01/persistencia01/Program.cs
+++ persistencia01/persistencia01/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: usuario1
+ * Usuario: diana
  * Fecha: 17/4/2026
  * Hora: 2:23 p. m.
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace persistencia01
 {
@@ -15,6 +16,22 @@ namespace persistencia01
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("hola diana");
+			
+			//directorio
+			
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Datos IUJO");
+			
+			string rutaReportes = Path.Combine(rutaRaiz, "REPORTE");
+			
+			Console.WriteLine (rutaRaiz);
+			
+		
+			Console.WriteLine (rutaReportes);
+			
+			if(!Directory.Exists (rutaReportes)) {
+				Directory.CreateDirectory(rutaReportes); 
+				Console.WriteLine("directorio creado carrectamente"); 
+			}
 			
 			// TODO: Implement Functionality Here
 			
